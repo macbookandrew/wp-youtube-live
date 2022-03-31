13be49e3ad9c95f58d0769c8536c57c36d6ad47d
Andrew Minion <andrew@andrewrminion.com>
2022-03-31 08:32:34 -0500

    update code style

--- inc/admin.php
+++ inc/admin.php
@@ -345,7 +345,7 @@ function refresh_youtube_live_upcoming_cache( $action = NULL, $nonce = NULL ) {
     }
 
     if ( ! wp_verify_nonce( $nonce, 'wpYTcache_nonce' ) ) {
-        die( 'Invalid nonce.');
+        die( 'Invalid nonce.' );
     }
 
     $youtube_options = get_option( 'youtube_live_settings' );
