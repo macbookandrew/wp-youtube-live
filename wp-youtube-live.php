9a35b2986e2f0b79fc340857e175a66075f59b7d
Andrew Minion <andrew@andrewrminion.com>
2024-01-15 20:17:26 -0600

    fix typo

--- wp-youtube-live.php
+++ wp-youtube-live.php
@@ -319,8 +319,8 @@ function wp_ytl_plugin_activation() {
 	}
 
 	// added in v1.7.0.
-	if ( ! array_key_exists( 'show_relatetd', $request_options ) ) {
-		$request_options['show_relatetd'] = false;
+	if ( ! array_key_exists( 'show_related', $request_options ) ) {
+		$request_options['show_related'] = false;
 	}
 
 	update_option( 'youtube_live_settings', $request_options );
